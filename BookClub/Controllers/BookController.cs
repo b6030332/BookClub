efaf4169037bb9772f384b3f0a3f3c12ad26690a
b6025922 <b6025922@my.shu.ac.uk>
2020-04-25 19:49:21 +0100

    Added Search Function

--- BookClub/Controllers/BookController.cs
+++ BookClub/Controllers/BookController.cs
@@ -1,9 +1,12 @@
-﻿using BookClub.Data.IDAO;
+﻿using BookClub.Data;
+using BookClub.Data.IDAO;
 using BookClub.Data.Models;
 using BookClub.Service.Service;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,16 +14,28 @@ namespace BookClub.Controllers
 {
     public class BookController : Controller
     {
+        private ApplicationDbContext _context;
         private IBookDAO _bookService;
         public BookController()
         {
+            _context = new ApplicationDbContext();
             _bookService = new BookService();
         }
-
-        public ActionResult GetAllBooks()
+        
+        public ActionResult GetAllBooks(string option, string search)
         {
-            IList<Book> books = _bookService.GetAllBooks();
-            return View("GetAllBooks", books);
+            if (option == "Genre")
+            {
+                return View(_context.Book.Where(b => b.Genre.Name == search || search == null).ToList());
+            }
+            else if (option == "Author")
+            {
+                return View(_context.Book.Where(b => b.Author.Name == search || search == null).ToList());
+            }
+            else
+            {
+                return View(_context.Book.Where(b => b.Title.StartsWith(search) || search == null).ToList());
+            }
         }
         public ActionResult GetBookId(int id)
         {
@@ -34,6 +49,10 @@ namespace BookClub.Controllers
             IList<Discussion> discussions = _bookService.GetDiscussionsByBook(id);
             return View("GetDiscussionsByBook", discussions);
          }
-
-        }
+        public ActionResult Index(string searching)
+        {
+            return View(_context.Book.Where(b => b.Title.Contains(searching) || searching == null).ToList());
+        } 
     }
+}
+    
