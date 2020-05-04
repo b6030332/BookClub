8361468dbc64624348139131841a037941a438eb
b6025922 <b6025922@my.shu.ac.uk>
2020-05-04 20:00:24 +0100

    Added favourites controller and heart icon

--- BookClub/Controllers/FavouritesController.cs
+++ BookClub/Controllers/FavouritesController.cs
@@ -1,6 +1,5 @@
 ﻿using BookClub.Data.IDAO;
 using BookClub.Data.Models;
-using BookClub.Models.Favourites;
 using BookClub.Service.Service;
 using System;
 using System.Collections.Generic;
@@ -18,21 +17,6 @@ namespace BookClub.Controllers
             _bookService = new BookService();
         }
             
-        // GET: Favourites
-        public ActionResult Index()
-        {
-            return View();
-        }
-        //public ActionResult AddBookToFavourites(int bookId)
-        //{
-        //    List<FavouriteBooks> favourites = new List<FavouriteBooks>();
-        //    Book book = _bookService.GetBookId(bookId);
-        //    favourites.Add(new FavouriteBooks()
-        //    { 
-        //        Book=book,
-
-        //    });
-
-        //}
+       
     }
 }
